48f4a78508019df488fd526aa5bc7051ac54d865
Malharii <152295830+malhariii@users.noreply.github.com>
2024-05-07 12:46:50 +0530

    util code learn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import app from "./app.js";
 import connectDB from "./db/index.js";
 
 import dotenv from 'dotenv'
@@ -8,3 +9,7 @@ import dotenv from 'dotenv'
 
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>console.log(`Server running on port ${process.env.PORT}`))
+})
+.catch((error)=>console.log("MONGODB connection failed",error))
